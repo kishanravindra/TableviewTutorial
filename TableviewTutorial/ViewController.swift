334362d9b0955afdd5789c8e092248b43f3a5f74
kishanravindra <rkishan@qwinixtech.com>
2015-01-27 03:34:35 -0600

    UITableView with Custom cell
    Added custom  tableview cell

--- TableviewTutorial/ViewController.swift
+++ TableviewTutorial/ViewController.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
+
+class CustomCell: UITableViewCell
+{
+    
+    @IBOutlet var countryImage: UIImageView!
+    @IBOutlet var CountryName: UILabel!
+}
+
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
 {
 
     @IBOutlet var tableView: UITableView!
+    let CellIdentifier: String = "customcell"
+
     var tableData:[String] = ["Qwinix", "Mysore", "Denver", "Dubai","Costa rica", "Swift"]
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier:"cell")
+       // self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier:"cell")
 
     }
 
@@ -38,11 +48,42 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         return tableData.count
     }
     
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return 80
+    }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
-        cell.textLabel?.text = tableData[indexPath.row]
-        return cell
+      //  var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        let cell: CustomCell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier) as CustomCell
+         cell.CountryName.text = tableData[indexPath.row]
+           switch indexPath.row
+           {
+               case 0:
+                cell.countryImage.image = UIImage (named:"qwinix-logo-fb.png")
+                
+               case 1:
+                cell.countryImage.image = UIImage (named:"Flag_of_India.png")
+                
+               case 2:
+                cell.countryImage.image = UIImage (named:"Flag_of_United_States.png")
+                
+               case 3:
+                cell.countryImage.image = UIImage (named:"Flag_of_Palestine.png")
+                
+               case 4:
+                cell.countryImage.image = UIImage (named:"Flag_of_Thailand.png")
+            
+              case 5:
+                cell.countryImage.image = UIImage (named:"Apple_Swift_Logo.png")
+            
+               default:
+                println("Over")
+        }
+          return cell
     }
     
 }
